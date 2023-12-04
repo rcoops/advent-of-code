573a86225e0e1558d030326001da12d4eec40d82
Rick Cooper <rick.cooper@krakenflex.com>
2023-12-04 22:34:16 +0000

    feat: day 2 step 1

---
+++ 2023/python/src/advent_of_code_2023/day_02/step_1.py
@@ -0,0 +1,59 @@
+from collections.abc import Generator
+from functools import reduce
+
+from advent_of_code_2023.resources import read_resource
+
+_COLOURS = ("red", "green", "blue")
+_COLOUR_COUNT_BASE = {"red": 0, "green": 0, "blue": 0}
+
+
+def count_colour(colour_count: str) -> tuple[str, int]:
+    _, num_str, colour = colour_count.split(" ")
+
+    return colour, int(num_str)
+
+
+def extract_set_counts(line: str) -> Generator[dict[str, int], None, None]:
+    return (
+        dict(count_colour(colours) for colours in game_set.split(","))
+        for game_set in line.split(";")
+    )
+
+
+def reduce_colour_counts(a: dict, b: dict) -> dict[str, int]:
+    return {key: max(a[key], b.get(key, 0)) for key in _COLOURS}
+
+
+def is_possible(line: str) -> bool:
+    set_counts = extract_set_counts(line)
+    totals = reduce(reduce_colour_counts, set_counts, _COLOUR_COUNT_BASE)
+
+    return totals["red"] <= 12 and totals["green"] <= 13 and totals["blue"] <= 14
+
+
+def extract_game_number(line: str) -> tuple[int, str]:
+    split = line.split(":")
+
+    return int(split[0][5:]), split[1]
+
+
+def extract_game_numbers(lines: list[str]) -> Generator[tuple[int, str], None, None]:
+    return (extract_game_number(line) for line in lines)
+
+
+def calculate_possible_games(input: str) -> int:
+    return sum(
+        (
+            game_number
+            for game_number, line in extract_game_numbers(input.splitlines())
+            if is_possible(line)
+        )
+    )
+
+
+def solve(resource: str):
+    input = read_resource(resource)
+
+    sum_of_possible_game_ids = calculate_possible_games(input)
+
+    return sum_of_possible_game_ids
