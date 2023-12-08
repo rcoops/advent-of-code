1af5273f1f785709357e9970e24f2159db2ccb90
Rick Cooper <rick.cooper@krakenflex.com>
2023-12-08 12:05:34 +0000

    refactor: less explicit for loops

--- 2023/python/src/advent_of_code_2023/day_03/step_1.py
+++ 2023/python/src/advent_of_code_2023/day_03/step_1.py
@@ -1,14 +1,11 @@
+from collections.abc import Callable
 from dataclasses import dataclass
-from typing import Callable
+from functools import reduce
+from typing import TypeAlias
 
-"""
-each group of digits
-what is its start & end index ()
-467..114..
-467 - 0, 2
-is any index start - 1 -> end+1 a symbol on line number -1 -> line number +1
-
-"""
+IndexCodePair: TypeAlias = tuple[int, str]
+IndexCodePairs: TypeAlias = list[IndexCodePair]
+OptionalInt: TypeAlias = tuple[int, ...]
 
 
 _DIGITS = "0123456789"
@@ -20,22 +17,60 @@ class EnginePart:
     value: int
     start_index: int
     end_index: int
+    symbol_search_indexes: tuple[int, ...]
 
 
-def _is_any_index_a_symbol(indexes: tuple[int, ...], input: str) -> bool:
+def _is_any_index_a_symbol(indexes: OptionalInt, input: str) -> bool:
     return any(input[i] for i in indexes if input[i] not in _NON_SYMBOLS)
 
 
 def _provide_calculate_bounded_index(
     max_input_index: int,
-) -> Callable[[int], tuple[int, ...]]:
+) -> Callable[[int], OptionalInt]:
     def calculate_bounded_index(index: int) -> tuple[int, ...]:
         return () if index <= 0 or index >= max_input_index else (index,)
 
     return calculate_bounded_index
 
 
-def _get_safe_range(begin: tuple[int, ...], end: tuple[int, ...]) -> tuple[int, ...]:
+def _provide_get_index_code_pairs(
+    input: str
+) -> Callable[[IndexCodePairs, IndexCodePair], IndexCodePairs]:
+    def reduce_index_part_code_pairs(
+        agg: IndexCodePairs,
+        next: IndexCodePair,
+    ) -> list[tuple[int, str]]:
+        i, c = next
+        if c not in _DIGITS:
+            return agg
+        if i == 0 or input[i - 1] not in _DIGITS:
+            return [*agg, next]
+        else:
+            last = agg[-1]
+            return [*agg[:-1], (last[0], f"{last[1]}{c}")]
+
+    return reduce_index_part_code_pairs
+
+
+def _provide_to_engine_part(
+    line_length: int,
+    calculate_bounded_index: Callable[[int], OptionalInt],
+) -> Callable[[tuple[int, str]], EnginePart]:
+    def to_engine_part(index_value_pair: tuple[int, str]) -> EnginePart:
+        index, value_text = index_value_pair
+        end_index = index + len(value_text)
+        symbol_search_indexes = _get_symbol_search_indexes(
+            index,
+            end_index,
+            line_length,
+            calculate_bounded_index,
+        )
+        return EnginePart(int(value_text), index, end_index, symbol_search_indexes)
+
+    return to_engine_part
+
+
+def _get_safe_range(begin: OptionalInt, end: OptionalInt) -> OptionalInt:
     if begin and end:
         return tuple(range(begin[0], end[0]))
     if begin:
@@ -43,17 +78,18 @@ def _get_safe_range(begin: tuple[int, ...], end: tuple[int, ...]) -> tuple[int, 
     return end if end else ()
 
 
-def _get_indexes(
-    part: EnginePart,
+def _get_symbol_search_indexes(
+    part_start_index: int,
+    part_end_index: int,
     line_length: int,
-    calculate_bounded_index: Callable[[int], tuple[int, ...]],
-) -> tuple[int, ...]:
-    previous_line_begin = calculate_bounded_index(part.start_index - line_length - 1)
-    previous_line_end = calculate_bounded_index(part.end_index - line_length + 1)
-    same_line_before = calculate_bounded_index(part.start_index - 1)
-    same_line_after = calculate_bounded_index(part.end_index)
-    next_line_begin = calculate_bounded_index(part.start_index + line_length - 1)
-    next_line_end = calculate_bounded_index(part.end_index + line_length + 1)
+    calculate_bounded_index: Callable[[int], OptionalInt],
+) -> OptionalInt:
+    previous_line_begin = calculate_bounded_index(part_start_index - line_length - 1)
+    previous_line_end = calculate_bounded_index(part_end_index - line_length + 1)
+    same_line_before = calculate_bounded_index(part_start_index - 1)
+    same_line_after = calculate_bounded_index(part_end_index)
+    next_line_begin = calculate_bounded_index(part_start_index + line_length - 1)
+    next_line_end = calculate_bounded_index(part_end_index + line_length + 1)
     return (
         *_get_safe_range(previous_line_begin, previous_line_end),
         *same_line_before,
@@ -65,27 +101,20 @@ def _get_indexes(
 def solve(input: str) -> int:
     line_length = input.index("\n") + 1
     max_input_index = len(input) - 1
-    potential_part_index_code_pairs: list[tuple[int, str]] = []
-    for i, c in enumerate(input):
-        if c in _DIGITS:
-            if i == 0 or input[i - 1] not in _DIGITS:
-                potential_part_index_code_pairs.append((i, c))
-            else:
-                potential_part_index_code_pairs[-1] = (
-                    potential_part_index_code_pairs[-1][0],
-                    f"{potential_part_index_code_pairs[-1][1]}{c}",
-                )
-    potential_parts = [
-        EnginePart(int(value), index, index + len(value))
-        for index, value in potential_part_index_code_pairs
-    ]
-
+    get_index_code_pairs = _provide_get_index_code_pairs(input)
     calculate_bounded_index = _provide_calculate_bounded_index(max_input_index)
-    index_tuples = [
-        (part, _get_indexes(part, line_length, calculate_bounded_index))
-        for part in potential_parts
-    ]
+    to_engine_part = _provide_to_engine_part(line_length, calculate_bounded_index)
+
+    index_potential_part_code_pairs = reduce(get_index_code_pairs, enumerate(input), [])
+    potential_parts = list(map(to_engine_part, index_potential_part_code_pairs))
+
     parts = (
-        part for part, indexes in index_tuples if _is_any_index_a_symbol(indexes, input)
+        part
+        for part in potential_parts
+        if _is_any_index_a_symbol(part.symbol_search_indexes, input)
     )
+
     return sum(part.value for part in parts)
+
+
+# Solution 525911
