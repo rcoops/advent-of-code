07bc08481841a7567c1b342a84b1714444842622
Rick Cooper <rick.cooper@krakenflex.com>
2023-12-08 21:16:46 +0000

    improvement: default slice as is faster

--- 2023/python/src/advent_of_code_2023/day_01/step_2/__init__.py
+++ 2023/python/src/advent_of_code_2023/day_01/step_2/__init__.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from time import perf_counter_ns
 
 from advent_of_code_2023.day_01.step_2.solve_index import (
     calculate_calibration_digits as calculate_calibration_digits_index,
@@ -27,12 +28,15 @@ def _calculate_calibration_value(line: str, method: Method) -> int:
     return left_digit * 10 + right_digit
 
 
-def solve(input: str, method_choice: str = "index"):
+def solve(input: str, method_choice: str = "slice"):
+    # Slice is faster
     method = Method(method_choice)
 
+    now = perf_counter_ns()
     total = sum(
         _calculate_calibration_value(line, method) for line in input.splitlines()
     )
+    print(perf_counter_ns() - now)
 
     return total
 
