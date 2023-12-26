9d190a26508bea45c3ccbad5264b02f279acd8d3
Rick Cooper <rickcooper84@gmail.com>
2023-12-26 14:48:56 +0000

    feat: day 4 step 1 attempt on example

--- 2023/python/src/advent_of_code_2023/day_04/step_1.py
+++ 2023/python/src/advent_of_code_2023/day_04/step_1.py
@@ -1,4 +1,34 @@
+from collections.abc import Callable
+from functools import reduce
+
+
+def provide_bitshift_if_present(
+    numbers_you_have: list[int],
+) -> Callable[[int, int], int]:
+    def bitshift_if_present(x: int, y: int) -> int:
+        return (x << 1) or 1 if y in numbers_you_have else x
+
+    return bitshift_if_present
+
+
+def extract_numbers(numbers_text: str) -> list[int]:
+    return [int(n) for n in numbers_text.split()]
+
+
+def solve_line(line: str) -> int:
+    if not line:
+        return 0
+    winning_numbers_text, numbers_you_have_text = line.split(":")[1].split("|")
+    winning_numbers = [int(n) for n in winning_numbers_text.split()]
+    numbers_you_have = [int(n) for n in numbers_you_have_text.split()]
+
+    bitshift_if_present = provide_bitshift_if_present(numbers_you_have)
+
+    return reduce(bitshift_if_present, winning_numbers, 0)
+
 
 def solve(input: str) -> int:
-    return 0
+    total = sum(solve_line(line) for line in input.splitlines())
+
+    return total
 
